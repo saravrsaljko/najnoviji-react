46f5d373617b875437747d39a4468e264fe699de
saravrsaljko <sara.vrsaljko@gmail.com>
2020-05-20 23:40:28 +0200

    Updates

---
+++ precache-manifest.b75ceb094d6ad2e2c46bca14aa351f2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1529a93929a2a2e6f2aaa5836ee4337",
+    "url": "/najnoviji-react/index.html"
+  },
+  {
+    "revision": "0adef19f86c47964d294",
+    "url": "/najnoviji-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dce9d1a6ecb8ee685842",
+    "url": "/najnoviji-react/static/js/2.a3a21694.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/najnoviji-react/static/js/2.a3a21694.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0adef19f86c47964d294",
+    "url": "/najnoviji-react/static/js/main.4cda8fe6.chunk.js"
+  },
+  {
+    "revision": "bed2ee0688fd121f9b52",
+    "url": "/najnoviji-react/static/js/runtime-main.de5b1c22.js"
+  }
+]);
